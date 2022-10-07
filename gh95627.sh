911639bfcda24e15d4f8ef4bb8cdf284a3d9eed9
ghh56627 <113602779+ghh56627@users.noreply.github.com>
2022-10-07 11:08:54 +0800

    Update gh95627.sh

--- gh95627.sh
+++ gh95627.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
 cp /opt/nvidia/nsight-systems/2022.1.3/host-linux-x64/libstdc++.so.6 /usr/lib/x86_64-linux-gnu/
 
 cd /content
 
 wget -c https://github.com/ghh56627/data_8391/raw/main/gh95627_train.ipynb
+
 wget -c https://github.com/ghh56627/data_8391/raw/main/gh95627_date.ipynb
 
 chmod 777 gh95627_train.ipynb
+
 chmod 777 gh95627_date.ipynb
-./gh95627_train.ipynb --user '初见' --password '483600' --lc0name 'gh95627_date.ipynb'
+
+./gh95627_train.ipynb --user 'tianya' --password 'wutao12345' --lc0name 'gh95627_date.ipynb'
